9e753440e19a7b0873e46d229ecb59042c7fbd2c
cag8xn <158217230+cag8xn@users.noreply.github.com>
2025-03-15 10:56:43 +0100

    Added blinds device. Not functional yet

---
+++ examples/blinds/main/app_driver.cpp
@@ -0,0 +1,332 @@
+/*
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <esp_log.h>
+#include <stdlib.h>
+#include <string.h>
+//#include "driver/ledc.h"
+#include "driver/mcpwm.h"
+#include "soc/mcpwm_periph.h"
+#include <device.h>
+#include <math.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include <app_reset.h>
+
+#include <esp_matter.h>
+#include "bsp/esp-bsp.h"
+
+#include <app_priv.h>
+
+using namespace chip::app::Clusters;
+using namespace esp_matter;
+using namespace esp_matter::cluster;
+
+static const char *TAG = "app_driver";
+extern uint16_t blinds_endpoint_id;
+
+// Define the number of servos
+#define NUM_SERVOS 4 
+
+// Define GPIO pin connected to the signal input of the servo
+//#define SERVO1_GPIO 13
+//#define SERVO2_GPIO 25
+//#define SERVO3_GPIO 33
+//#define SERVO4_GPIO 32
+
+#define SERVO1_GPIO 13
+#define SERVO2_GPIO 12
+#define SERVO3_GPIO 14
+#define SERVO4_GPIO 25
+
+#define release_open        20  //OK
+#define release_locked      130 //OK
+#define tensioner_loose     30  //OK
+#define tensioner_charged   110 //OK
+#define lid_closed          0   //OK
+#define lid_open            150//165 //OK
+#define lid_reload          115
+#define reload_closed       125 //OK
+#define reload_open         30  //OK
+
+// Define servo names
+const char* servo_names[NUM_SERVOS] = {"Release", "Tensioner", "Lid", "Reload"};
+
+// Define GPIO pins for each servo
+const int servo_pins[NUM_SERVOS] = {SERVO1_GPIO, SERVO2_GPIO, SERVO3_GPIO, SERVO4_GPIO};
+
+// Define two separate units
+const mcpwm_unit_t servo_pwm_unit[NUM_SERVOS] = {MCPWM_UNIT_0, MCPWM_UNIT_1, MCPWM_UNIT_0, MCPWM_UNIT_1};
+
+// Define two separate timers
+const mcpwm_timer_t servo_pwm_timer[NUM_SERVOS] = {MCPWM_TIMER_0, MCPWM_TIMER_0, MCPWM_TIMER_1, MCPWM_TIMER_1};
+
+// Define MCPWM output signals for each servo
+const mcpwm_io_signals_t servo_pwm_signals[NUM_SERVOS] = {MCPWM0A, MCPWM0B, MCPWM1A, MCPWM1B};
+
+// Define MCPWM generators for each servo
+const mcpwm_generator_t servo_pwm_generator[NUM_SERVOS] = {MCPWM_OPR_A, MCPWM_OPR_B, MCPWM_OPR_A, MCPWM_OPR_B};
+
+
+// Define parameters for the servo motor
+#define SERVO_MIN_PULSEWIDTH 500  // Minimum pulse width in microseconds
+#define SERVO_MAX_PULSEWIDTH 2500 // Maximum pulse width in microseconds
+#define SERVO_MAX_DEGREE 180       // Maximum angle in degrees servo can rotate
+
+// ELIA: Following code TOGGLES the OnOff attribute.
+static void toggle_OnOff_attribute() {
+    uint16_t endpoint_id = blinds_endpoint_id;
+    uint32_t cluster_id = OnOff::Id;
+    uint32_t attribute_id = OnOff::Attributes::OnOff::Id;
+
+    node_t *node = node::get();
+    endpoint_t *endpoint = endpoint::get(node, endpoint_id);
+    cluster_t *cluster = cluster::get(endpoint, cluster_id);
+    attribute_t *attribute = attribute::get(cluster, attribute_id);
+
+    esp_matter_attr_val_t val = esp_matter_invalid(NULL);
+    attribute::get_val(attribute, &val);
+    val.val.b = !val.val.b;
+    attribute::update(endpoint_id, cluster_id, attribute_id, &val);
+}
+
+/* Do any conversions/remapping for the actual value here */
+/*ELIA MODS*/
+int angle_to_duty(int angle){
+    int result = static_cast<int>((static_cast<double>(angle) / SERVO_MAX_DEGREE) * (SERVO_MAX_PULSEWIDTH - SERVO_MIN_PULSEWIDTH) + SERVO_MIN_PULSEWIDTH);
+    return result;
+}
+
+static void drive_servo_smoothly(int servo_id, int start, int end, int total_delay) {
+    // ALERT: delay_time_ms cannot be less than 10!! A certain value of num_steps implies a certain minimum total delay!!
+    const int num_steps = 100;
+    const int delay_time_ms = total_delay / num_steps;
+    const int start_duty = angle_to_duty(start);
+    const int end_duty = angle_to_duty(end);
+    
+    // Iterate through a number of steps to smoothly change the duty cycle from start to end
+    // The angle varies from 0 to pi/2, creating a sinusoidal increment in duty cycle
+    for (int i = 0; i < num_steps; i++) {
+        double angle = M_PI/2 * i / (num_steps - 1); // Angle ranges from 0 to pi/2
+        double duty = sin(angle) * sin(angle) * (end_duty - start_duty) + start_duty;
+        mcpwm_set_duty_in_us(servo_pwm_unit[servo_id], servo_pwm_timer[servo_id], servo_pwm_generator[servo_id], duty);
+        // Wait for a short duration
+        vTaskDelay(delay_time_ms / portTICK_PERIOD_MS);
+    }
+}
+
+static void drive_servo_linearly(int servo_id, int start, int end, int total_delay) {
+    // ALERT: delay_time_ms cannot be less than 10!! A certain value of num_steps implies a certain minimum total delay!!
+    const int num_steps = 100;
+    const int delay_time_ms = total_delay / num_steps;
+    const int start_duty = angle_to_duty(start);
+    const int end_duty = angle_to_duty(end);
+    
+    // Iterate through a number of steps to linearly change the duty cycle from start to end
+    for (int i = 0; i < num_steps; i++) {
+        //double duty = i / num_steps * (end_duty - start_duty) + start_duty;
+        double duty = static_cast<double>(i) / num_steps * (end_duty - start_duty) + start_duty;
+        mcpwm_set_duty_in_us(servo_pwm_unit[servo_id], servo_pwm_timer[servo_id], servo_pwm_generator[servo_id], duty);
+        // Wait for a short duration
+        vTaskDelay(delay_time_ms / portTICK_PERIOD_MS);
+    }
+}
+
+static void launch(void *pvParameters) {
+    mcpwm_config_t pwm_config;
+    pwm_config.frequency = 50;  // Set frequency to 50Hz, suitable for most servos
+    pwm_config.cmpr_a = 0;      // Initial duty cycle
+    pwm_config.counter_mode = MCPWM_UP_COUNTER;
+    pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
+    //mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);
+    //mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_1, &pwm_config);
+
+    // Initialize and configure MCPWM for each servo
+    for (int servo_id = 0; servo_id < NUM_SERVOS; servo_id++) {
+        ESP_LOGI(TAG, "Initializing %s with GPIO pin %d", servo_names[servo_id], servo_pins[servo_id]);
+        mcpwm_gpio_init(servo_pwm_unit[servo_id], servo_pwm_signals[servo_id], servo_pins[servo_id]);
+        // Use separate timers for each servo
+        mcpwm_init(servo_pwm_unit[servo_id], servo_pwm_timer[servo_id], &pwm_config);
+        mcpwm_set_duty_in_us(servo_pwm_unit[servo_id], servo_pwm_timer[servo_id], servo_pwm_generator[servo_id], 0);
+    }
+
+    //vTaskDelay(5000 / portTICK_PERIOD_MS);
+    int duty = angle_to_duty(tensioner_loose);
+    mcpwm_set_duty_in_us(servo_pwm_unit[1], servo_pwm_timer[1], servo_pwm_generator[1], duty);
+    duty = angle_to_duty(reload_closed);
+    mcpwm_set_duty_in_us(servo_pwm_unit[3], servo_pwm_timer[3], servo_pwm_generator[3], duty);
+    vTaskDelay(300 / portTICK_PERIOD_MS);
+    duty = angle_to_duty(release_locked);
+    mcpwm_set_duty_in_us(servo_pwm_unit[0], servo_pwm_timer[0], servo_pwm_generator[0], duty);
+    duty = angle_to_duty(lid_closed);
+    mcpwm_set_duty_in_us(servo_pwm_unit[2], servo_pwm_timer[2], servo_pwm_generator[2], duty);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+    // Open lid
+    drive_servo_smoothly(2, lid_closed, lid_open, 1500);
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+
+    // Charge tensioner
+    drive_servo_linearly(1, tensioner_loose, tensioner_charged, 1000);
+    vTaskDelay(200 / portTICK_PERIOD_MS);
+
+    // Release lock
+    drive_servo_linearly(0, release_locked, release_open, 1000);
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+
+    // Fold tensioner
+    drive_servo_smoothly(1, tensioner_charged, tensioner_loose, 1000);
+    vTaskDelay(300 / portTICK_PERIOD_MS);
+
+    // Lock the release and toggle reload
+    duty = angle_to_duty(release_locked);
+    mcpwm_set_duty_in_us(servo_pwm_unit[0], servo_pwm_timer[0], servo_pwm_generator[0], duty);
+    duty = angle_to_duty(reload_open);
+    mcpwm_set_duty_in_us(servo_pwm_unit[3], servo_pwm_timer[3], servo_pwm_generator[3], duty);
+    vTaskDelay(300 / portTICK_PERIOD_MS);
+    duty = angle_to_duty(reload_closed);
+    mcpwm_set_duty_in_us(servo_pwm_unit[3], servo_pwm_timer[3], servo_pwm_generator[3], duty);
+    vTaskDelay(300 / portTICK_PERIOD_MS);
+
+    // Tilt lid to reload
+    duty = angle_to_duty(lid_reload);
+    mcpwm_set_duty_in_us(servo_pwm_unit[2], servo_pwm_timer[2], servo_pwm_generator[2], duty);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+
+    // Close lid
+    drive_servo_smoothly(2, lid_reload, lid_closed, 2000);
+
+    // Stop PWM generation for each servo
+    for (int servo_id = 0; servo_id < NUM_SERVOS; servo_id++) {
+        mcpwm_set_duty_in_us(servo_pwm_unit[servo_id], servo_pwm_timer[servo_id], servo_pwm_generator[servo_id], 0);
+        mcpwm_stop(servo_pwm_unit[servo_id], servo_pwm_timer[servo_id]);
+    }
+    // Delete the task from within the function
+    vTaskDelete(NULL);
+}
+
+static esp_err_t app_driver_blinds_set_power(led_indicator_handle_t handle, esp_matter_attr_val_t *val)
+{
+    if (val->val.b) {
+        ESP_LOGI(TAG, "================================== Catapult power ON signal received: %d", val->val.b);
+        xTaskCreate(&launch,         // Function to run
+                "launch_task",   // Task name
+                4096,                 // Stack size (in words, not bytes)
+                NULL,                 // No parameter to pass to the task
+                5,                    // Priority (higher number means higher priority)
+                NULL);                // Task handle (can be NULL if not needed)
+
+        //launch();
+
+        ESP_LOGI(TAG, "ELIA ================================== Attempting to turn OFF the OnOff attribute");
+        toggle_OnOff_attribute();
+    }
+    return ESP_OK;
+}
+
+static esp_err_t app_driver_light_set_brightness(led_indicator_handle_t handle, esp_matter_attr_val_t *val)
+{
+    int value = REMAP_TO_RANGE(val->val.u8, MATTER_BRIGHTNESS, STANDARD_BRIGHTNESS);
+#if BSP_LED_NUM > 0
+    return led_indicator_set_brightness(handle, value);
+#else
+    ESP_LOGI(TAG, "LED set brightness: %d", value);
+    return ESP_OK;
+#endif
+}
+
+static void app_driver_button_toggle_cb(void *arg, void *data)
+{
+    ESP_LOGI(TAG, "Toggle button pressed");
+    toggle_OnOff_attribute();
+}
+
+esp_err_t app_driver_attribute_update(app_driver_handle_t driver_handle, uint16_t endpoint_id, uint32_t cluster_id,
+                                      uint32_t attribute_id, esp_matter_attr_val_t *val)
+{
+    ESP_LOGI(TAG, "8===============================D In the driver");
+    ESP_LOGI(TAG, "8===============================D : endpoint_id = %d, cluster_id = %lu, attribute_id = %lu", endpoint_id, cluster_id, attribute_id);
+    ESP_LOGI(TAG, "8===============================D : endpoint_id = %d, blinds_endpoint_id = %d", endpoint_id, blinds_endpoint_id);
+    esp_err_t err = ESP_OK;
+    if (endpoint_id == blinds_endpoint_id) {
+        ESP_LOGI(TAG, "8===============================D Received endpoint message");
+        led_indicator_handle_t handle = (led_indicator_handle_t)driver_handle;
+        if (cluster_id == WindowCovering::Id) {
+            ESP_LOGI(TAG, "8===============================D Received cluster message");
+            if (attribute_id == WindowCovering::Attributes::CurrentPositionLift::Id) {
+                // Set the blinds position based on the attribute value
+                // err = app_driver_blinds_set_position(handle, val);
+                ESP_LOGI("CurrentPosition", "Received attribute update: endpoint_id = %d, cluster_id = %lu, attribute_id = %lu", endpoint_id, cluster_id, attribute_id);
+            } else if (attribute_id == WindowCovering::Attributes::CurrentPositionLiftPercentage::Id) {
+                // Set the target position for the blinds
+                // err = app_driver_blinds_set_target_position(handle, val);
+                ESP_LOGI("CurrentPositionLiftPercentage", "Received attribute update: endpoint_id = %d, cluster_id = %lu, attribute_id = %lu", endpoint_id, cluster_id, attribute_id);
+            } else if (attribute_id == WindowCovering::Attributes::TargetPositionLiftPercent100ths::Id) {
+                // Adjust the lift percentage if needed
+                // err = app_driver_blinds_set_lift_percentage(handle, val);
+                ESP_LOGI(TAG, "8===============================D Received attribute update: TargetPositionLiftPercent100ths");
+            } else if (attribute_id == WindowCovering::Attributes::CurrentPositionLiftPercent100ths::Id) {
+                // Set the target lift percentage
+                // err = app_driver_blinds_set_target_lift_percentage(handle, val);
+                ESP_LOGI("CurrentPositionLiftPercent100ths", "Received attribute update: endpoint_id = %d, cluster_id = %lu, attribute_id = %lu", endpoint_id, cluster_id, attribute_id);
+            }
+        }
+    }
+    return err;
+}
+
+
+app_driver_handle_t app_driver_blinds_init()
+{
+#if BSP_LED_NUM > 0
+    /* Initialize led */
+    led_indicator_handle_t leds[BSP_LED_NUM];
+    ESP_ERROR_CHECK(bsp_led_indicator_create(leds, NULL, BSP_LED_NUM));
+    led_indicator_set_hsv(leds[0], SET_HSV(DEFAULT_HUE, DEFAULT_SATURATION, DEFAULT_BRIGHTNESS));
+    
+    return (app_driver_handle_t)leds[0];
+#else
+    return NULL;
+#endif
+}
+
+app_driver_handle_t app_driver_button_init()
+{
+    /* Initialize button */
+    button_config_t config = {
+        .type = BUTTON_TYPE_GPIO,
+        .gpio_button_config = {
+            .gpio_num = 17,
+            .active_level = 0,
+        }
+    };
+    button_handle_t handle = iot_button_create(&config);
+    iot_button_register_cb(handle, BUTTON_PRESS_DOWN, app_driver_button_toggle_cb, NULL);
+    gpio_set_pull_mode((gpio_num_t)17, GPIO_PULLUP_ONLY);
+    
+    return (app_driver_handle_t)handle;
+}
+
+app_driver_handle_t app_driver_reset_button_init()
+{
+    /* Initialize button */
+    button_config_t config = {
+        .type = BUTTON_TYPE_GPIO,
+        .gpio_button_config = {
+            .gpio_num = 16,
+            .active_level = 0,
+        }
+    };
+    button_handle_t handle = iot_button_create(&config);
+    app_reset_button_register(handle);
+
+    // Enable internal pull-up resistor for GPIO 17
+    gpio_set_pull_mode((gpio_num_t)16, GPIO_PULLUP_ONLY);
+    
+    return (app_driver_handle_t)handle;
+}
---
+++ examples/blinds/main/app_main.cpp
@@ -0,0 +1,187 @@
+/*
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <esp_err.h>
+#include <esp_log.h>
+#include <nvs_flash.h>
+
+#include <esp_matter.h>
+#include <esp_matter_cluster.h>
+#include <esp_matter_endpoint.h>
+#include <esp_matter_console.h>
+#include <esp_matter_ota.h>
+
+#include <common_macros.h>
+#include <app_priv.h>
+#include <app_reset.h>
+#if CHIP_DEVICE_CONFIG_ENABLE_THREAD
+#include <platform/ESP32/OpenthreadLauncher.h>
+#endif
+
+#include <app/server/CommissioningWindowManager.h>
+#include <app/clusters/diagnostic-logs-server/diagnostic-logs-server.h>
+#include <diagnostic-logs-provider-delegate-impl.h>
+#include <app/server/Server.h>
+
+static const char *TAG = "app_main";
+uint16_t blinds_endpoint_id = 0;
+
+using namespace esp_matter;
+using namespace esp_matter::attribute;
+using namespace esp_matter::endpoint;
+using namespace chip::app::Clusters;
+
+constexpr auto k_timeout_seconds = 300;
+
+#if CONFIG_ENABLE_ENCRYPTED_OTA
+extern const char decryption_key_start[] asm("_binary_esp_image_encryption_key_pem_start");
+extern const char decryption_key_end[] asm("_binary_esp_image_encryption_key_pem_end");
+
+static const char *s_decryption_key = decryption_key_start;
+static const uint16_t s_decryption_key_len = decryption_key_end - decryption_key_start;
+#endif // CONFIG_ENABLE_ENCRYPTED_OTA
+
+static void app_event_cb(const ChipDeviceEvent *event, intptr_t arg)
+{
+    switch (event->Type) {
+    case chip::DeviceLayer::DeviceEventType::kInterfaceIpAddressChanged:
+        ESP_LOGI(TAG, "Interface IP Address changed");
+        break;
+
+    case chip::DeviceLayer::DeviceEventType::kCommissioningComplete:
+        ESP_LOGI(TAG, "Commissioning complete");
+        break;
+
+    case chip::DeviceLayer::DeviceEventType::kFailSafeTimerExpired:
+        ESP_LOGI(TAG, "Commissioning failed, fail safe timer expired");
+        break;
+
+    case chip::DeviceLayer::DeviceEventType::kCommissioningSessionStarted:
+        ESP_LOGI(TAG, "Commissioning session started");
+        break;
+
+    case chip::DeviceLayer::DeviceEventType::kCommissioningSessionStopped:
+        ESP_LOGI(TAG, "Commissioning session stopped");
+        break;
+
+    case chip::DeviceLayer::DeviceEventType::kCommissioningWindowOpened:
+        ESP_LOGI(TAG, "Commissioning window opened");
+        break;
+
+    case chip::DeviceLayer::DeviceEventType::kCommissioningWindowClosed:
+        ESP_LOGI(TAG, "Commissioning window closed");
+        break;
+
+    case chip::DeviceLayer::DeviceEventType::kFabricRemoved:
+        {
+            ESP_LOGI(TAG, "Fabric removed successfully");
+            if (chip::Server::GetInstance().GetFabricTable().FabricCount() == 0)
+            {
+                chip::CommissioningWindowManager & commissionMgr = chip::Server::GetInstance().GetCommissioningWindowManager();
+                constexpr auto kTimeoutSeconds = chip::System::Clock::Seconds16(k_timeout_seconds);
+                if (!commissionMgr.IsCommissioningWindowOpen())
+                {
+                    /* After removing last fabric, this example does not remove the Wi-Fi credentials
+                     * and still has IP connectivity so, only advertising on DNS-SD.
+                     */
+                    CHIP_ERROR err = commissionMgr.OpenBasicCommissioningWindow(kTimeoutSeconds,
+                                                    chip::CommissioningWindowAdvertisement::kDnssdOnly);
+                    if (err != CHIP_NO_ERROR)
+                    {
+                        ESP_LOGE(TAG, "Failed to open commissioning window, err:%" CHIP_ERROR_FORMAT, err.Format());
+                    }
+                }
+            }
+        break;
+        }
+
+    case chip::DeviceLayer::DeviceEventType::kFabricWillBeRemoved:
+        ESP_LOGI(TAG, "Fabric will be removed");
+        break;
+
+    case chip::DeviceLayer::DeviceEventType::kFabricUpdated:
+        ESP_LOGI(TAG, "Fabric is updated");
+        break;
+
+    case chip::DeviceLayer::DeviceEventType::kFabricCommitted:
+        ESP_LOGI(TAG, "Fabric is committed");
+        break;
+
+    case chip::DeviceLayer::DeviceEventType::kBLEDeinitialized:
+        ESP_LOGI(TAG, "BLE deinitialized and memory reclaimed");
+        break;
+
+    default:
+        break;
+    }
+}
+
+// This callback is invoked when clients interact with the Identify Cluster.
+// In the callback implementation, an endpoint can identify itself. (e.g., by flashing an LED or light).
+static esp_err_t app_identification_cb(identification::callback_type_t type, uint16_t endpoint_id, uint8_t effect_id,
+                                       uint8_t effect_variant, void *priv_data)
+{
+    ESP_LOGI(TAG, "Identification callback: type: %u, effect: %u, variant: %u", type, effect_id, effect_variant);
+    return ESP_OK;
+}
+
+// This callback is called for every attribute update. The callback implementation shall
+// handle the desired attributes and return an appropriate error code. If the attribute
+// is not of your interest, please do not return an error code and strictly return ESP_OK.
+static esp_err_t app_attribute_update_cb(attribute::callback_type_t type, uint16_t endpoint_id, uint32_t cluster_id,
+                                         uint32_t attribute_id, esp_matter_attr_val_t *val, void *priv_data)
+{
+    esp_err_t err = ESP_OK;
+
+    //ELIA: Changing to POST_UPDATE simply made the callback to be executed after the attribute is updated
+    if (type == PRE_UPDATE) {
+        /* Driver update */
+        app_driver_handle_t driver_handle = (app_driver_handle_t)priv_data;
+        err = app_driver_attribute_update(driver_handle, endpoint_id, cluster_id, attribute_id, val);
+    }
+
+    return err;
+}
+
+extern "C" void app_main()
+{
+    esp_err_t err = ESP_OK;
+
+    /* Initialize the ESP NVS layer */
+    nvs_flash_init();
+
+esp_matter::node::config_t node_config;
+esp_matter::node_t *node = esp_matter::node::create(&node_config, app_attribute_update_cb, NULL);
+esp_matter::endpoint::window_covering_device::config_t window_config;
+esp_matter::endpoint_t *endpoint = esp_matter::endpoint::window_covering_device::create(node, &window_config, esp_matter::endpoint_flags::ENDPOINT_FLAG_NONE, NULL);
+blinds_endpoint_id = endpoint::get_id(endpoint);
+ESP_LOGI(TAG, "Blinds created with endpoint_id %d", blinds_endpoint_id);
+
+esp_matter::cluster_t *cluster = esp_matter::cluster::get(endpoint, chip::app::Clusters::WindowCovering::Id);
+esp_matter::cluster::window_covering::feature::lift::config_t lift;
+esp_matter::cluster::window_covering::feature::position_aware_lift::config_t position_aware_lift;
+
+nullable<uint8_t> percentage = nullable<uint8_t>(0);
+nullable<uint16_t> percentage_100ths = nullable<uint16_t>(0);
+position_aware_lift.current_position_lift_percentage = percentage;
+position_aware_lift.target_position_lift_percent_100ths = percentage_100ths;
+position_aware_lift.current_position_lift_percent_100ths = percentage_100ths;
+
+esp_matter::cluster::window_covering::feature::absolute_position::config_t absolute_position;
+esp_matter::cluster::window_covering::feature::lift::add(cluster, &lift);
+esp_matter::cluster::window_covering::feature::position_aware_lift::add(cluster, &position_aware_lift);
+esp_matter::cluster::window_covering::feature::absolute_position::add(cluster, &absolute_position);
+
+// add diagnostic logs cluster on root endpoint
+esp_matter::cluster::diagnostic_logs::config_t diag_logs_config;
+esp_matter::endpoint_t *root_ep = endpoint::get(node, 0); // get the root node ep
+esp_matter::cluster::diagnostic_logs::create(root_ep, &diag_logs_config, CLUSTER_FLAG_SERVER);
+
+esp_matter::start(app_event_cb);
+
+}
